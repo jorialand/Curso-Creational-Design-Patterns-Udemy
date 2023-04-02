d50a14670339173d33e637588ae19234e9055afb
HoriaGhionoiuMartinez <horia.ghionoiu@alma-medical.com>
2023-04-02 22:00:36 +0100

    Exercise: logger - Error, multiple Logger instances.

--- 0_Singleton/Singleton/Ejercicios/main.cpp
+++ 0_Singleton/Singleton/Ejercicios/main.cpp
@@ -1,10 +1,18 @@
 #include "Logger.h"
 
+void OpenConnection()
+{
+	//Logger& log = Logger::Instance();
+	Logger log;
+	log.WriteLog("OpenConnection");
 
+}
 int main()
 {
-	Logger& log = Logger::Instance();
+	//Logger& log = Logger::Instance();
+	Logger log;
 	log.SetTag("INFO");
 	log.WriteLog("App started.");
+	OpenConnection();
 	log.WriteLog("App shutting down.");
 }

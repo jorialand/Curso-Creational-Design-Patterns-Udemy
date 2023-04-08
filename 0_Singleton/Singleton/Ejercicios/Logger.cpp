ebfdb5dc06042149babfdfeeae9cb8895863d2ef
HoriaGhionoiuMartinez <horia.ghionoiu@alma-medical.com>
2023-04-08 19:24:44 +0100

    Exercise: logger - Multithreading. As the compiler could write instructions in any order, while creating the new logger instance, this may crash. Created manually the three steps to create a new logger instance, and crashed.

--- 0_Singleton/Singleton/Ejercicios/Logger.cpp
+++ 0_Singleton/Singleton/Ejercicios/Logger.cpp
@@ -24,7 +24,13 @@ Logger& Logger::Instance()
 	{
 		m_mutex.lock();
 		if (nullptr == m_pInstance)
-			m_pInstance = new Logger();
+		{
+			//m_pInstance = new Logger(); // This operation is splitted in 3, by the compiler.
+			void* p = operator new(sizeof(Logger)); // Step 1 Memory allocation.
+			m_pInstance = static_cast<Logger*>(p);// Step 3 Memory address assigned.
+			new(p) Logger{}; // Step 2 Initialization.
+			// The compiler can choose to reorder the instructions.
+		}
 		m_mutex.unlock();
 	}
 	return *m_pInstance;
